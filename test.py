c67e98bbe8897598be16af17a7200f7e7c2487f5
Harry Westmoreland <harry.westmoreland2002@gmail.com>
2022-01-23 11:05:12 +0000

    created a test to test connect_node function

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 '''This is a test Module for the Project'''
-from main import read_tgf_file
-
+from main import read_tgf_file, Node
 def test_example_read():
     '''Tests read_tgf_file on an example file'''
     nodes = read_tgf_file("example.tgf")
@@ -9,3 +8,14 @@ def test_example_read():
     assert nodes[1].connected_nodes[0][1] == 8
     assert nodes[1].connected_nodes[1][0].name == "Liverpool"
     assert nodes[1].connected_nodes[1][1] == 9
+
+def test_connect_node():
+    '''tests node connection functionality'''
+    node_a = Node("node_a")
+    node_b = Node("node_b")
+
+    node_a.connect_node(node_b,5)
+
+    assert node_a.connected_nodes[0][0] == node_b
+    assert node_a.connected_nodes[0][1] == 5
+    
